91db39e2c5f58e36f1236a1aa5400d8bef390bd6
Nadia <nadia.kuraina12@gmail.com>
2025-05-17 18:27:34 +0300

    add products

---
+++ popup.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('btn-panel')
+    const mask = document.getElementById('mask-panel')
+    const content = document.getElementById('menu-panel')
+    const close = document.getElementById('close-panel')
+
+    if (btn && mask && content && close) {
+        btn.addEventListener('click', () => {
+            mask.style.display = 'block'
+            content.style.display = 'block'
+        })
+        mask.addEventListener('click', () => {
+            mask.style.display = 'none'
+            content.style.display = 'none'
+        })
+        close.addEventListener('click', () => {
+            mask.style.display = 'none'
+            content.style.display = 'none'
+        })
+        for (let e of content.getElementsByTagName('a')) {
+            e.addEventListener('click', () => {
+                mask.style.display = 'none'
+                content.style.display = 'none'
+            })
+        }
+    }
+
+    const popup = document.getElementById('popup-message')
+    const btns = document.querySelectorAll('.btn-popup')
+    const closePopup = document.getElementById('btn-close')
+    const closeBlock = document.getElementById('block-close')
+
+    if (popup && closePopup && closeBlock && btns.length > 0) {
+        for (let btn of btns) {
+            btn.addEventListener('click', () => {
+                popup.style.display = 'block'
+                closeBlock.style.display = 'block'
+            })
+        }
+        closePopup.addEventListener('click', () => {
+            popup.style.display = 'none'
+            closeBlock.style.display = 'none'
+        })
+        closeBlock.addEventListener('click', () => {
+            popup.style.display = 'none'
+            closeBlock.style.display = 'none'
+        })
+    }
+})
